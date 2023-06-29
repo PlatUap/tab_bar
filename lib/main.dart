3f5919356942690cc6da2f9057815d5d8cd45d70
PlatUap <platuap@gmail.com>
2023-06-29 23:13:03 +0700

    commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Contoh Tab Bar"),
+            bottom: const TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.comment),
+                  text: "Comments",
+                ),
+                Tab(
+                  child: Image(
+                    image: NetworkImage("https://picsum.photos/200"),
+                  ),
+                ),
+                Tab(
+                  icon: Icon(Icons.computer),
+                ),
+                Tab(
+                  text: "News",
+                ),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: <Widget>[
+              Center(
+                child: Text("Tab 1"),
+              ),
+              Center(
+                child: Text("Tab 2"),
+              ),
+              Center(
+                child: Text("Tab 3"),
+              ),
+              Center(
+                child: Text("Tab 4"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
